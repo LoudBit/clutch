7d7976831885d663767142e88122aa7335702f07
Darcy Murphy <mrDarcyMurphy@gmail.com>
2014-07-12 16:33:22 -0500

    removed commented lines

--- js/controllers/color-controller.js
+++ js/controllers/color-controller.js
@@ -3,13 +3,6 @@ clutch.controller('ColorCtrl', ['$scope', 'Color', 'Spectrum', 'Grid', 'Anchor',
 
   $scope.anchor = Anchor
 
-  // $scope.rgb = Color.rgb({r:255, g:0, b:0})
-
-  // $scope.lch = Color.lch({l:50, c:50, h:0})
-
-  // $scope.spectrum = Spectrum.create()
-
-  // $scope.grid = Grid.create()
-
   return this
+
 }])
