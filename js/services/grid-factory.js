415b6de7cf928bbbe51e162c33c11fb261cc0419
Darcy Murphy <mrDarcyMurphy@gmail.com>
2014-08-18 17:11:08 -0500

    linting better

--- js/services/grid-factory.js
+++ js/services/grid-factory.js
@@ -5,7 +5,6 @@ clutch.factory('Grid', ['Anchor', 'Spectrum', function(Anchor, Spectrum) {
 
     styl.push(_.map(Grid.colors, function(row){
       return _.map(row, function(color){
-        console.log(color.hex, color.lch.l, color.lch.c, color.lch.h)
         return {
           bottom: color.lch.l + '%'
         }
@@ -18,14 +17,20 @@ clutch.factory('Grid', ['Anchor', 'Spectrum', function(Anchor, Spectrum) {
   function createGrid(lch) {
     var i, l, lOffset, grid = []
 
+    var defaults = {
+      l: 50,
+      c: 50,
+      h: 180
+    }
+
     lch = lch && _.defaults(lch, defaults) || defaults
-    l = 100 / Grid.lightnesses.length
+    l = 100 / Grid.lights.length
     lOffset = lch.l % l
 
-    for (i = 0; i < Grid.lightnesses.length; i++) {
+    for (i = 0; i < Grid.lights.length; i++) {
 
       grid.unshift(Spectrum.create({
-        l: Grid.lightnesses[i],
+        l: Grid.lights[i],
         c: lch.c,
         h: lch.h
       }))
@@ -34,39 +39,34 @@ clutch.factory('Grid', ['Anchor', 'Spectrum', function(Anchor, Spectrum) {
     return grid
   }
 
-  function lightnesses(lch) {
-    var l = 100 / Grid.lightnesses.length
+  function lights(lch) {
+    var l = 100 / Grid.lights.length
     var lOffset = lch.l % l
-    var lightnesses = []
+    var output = []
 
     _.times(Grid.rows, function(i) {
-      lightnesses.push(l * i + lOffset)
+      output.push(l * i + lOffset)
     })
 
-    return lightnesses
-  }
-
-  var defaults = {
-    l: 50,
-    c: 50,
-    h: 180
+    return output
   }
 
   var Grid = {
     rows: 3,
-    lightnesses: [75, 50, 25],
+    lights: [75, 50, 25],
     colors: [[]],
     create: createGrid,
     update: function() {
       Anchor.updateLch()
+      Grid.rows = parseInt(Grid.rows, 10)
+      Grid.lights = lights(Anchor.color.lch)
       Grid.colors = createGrid(Anchor.color.lch)
-      Grid.lightnesses = lightnesses(Anchor.color.lch)
       Grid.styles = stylize()
     }
   }
 
+  Grid.lights = lights(Anchor.color.lch)
   Grid.colors = createGrid(Anchor.color.lch)
-  Grid.lightnesses = lightnesses(Anchor.color.lch)
   Grid.styles = stylize()
 
   return Grid
