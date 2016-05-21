f4a2a6299bdc3d37578a8a5c01115f8d3d64fc3b
Darcy Murphy <mrDarcyMurphy@gmail.com>
2016-05-21 13:18:04 -0500

    initial ugly as fuck gradient implementation

--- js/services/anchor-factory.js
+++ js/services/anchor-factory.js
@@ -1,5 +1,81 @@
 clutch.factory('Anchor', ['Color', function(Color) {
 
+  // This could be performance intensive.
+  // Be sure to test the performance impact during development.
+  // Should be extracted to a factory once it's ready.
+  function createGradients(color) {
+    var l = [
+      Color.lch({
+        l: 0,
+        c: color.lch.c,
+        h: color.lch.h
+      }),
+      color,
+      Color.lch({
+        l: 100,
+        c: color.lch.c,
+        h: color.lch.h
+      })
+    ];
+
+    var c = [
+      Color.lch({
+        l: color.lch.l,
+        c: 0,
+        h: color.lch.h
+      }),
+      color,
+      Color.lch({
+        l: color.lch.l,
+        c: 100,
+        h: color.lch.h
+      })
+    ];
+
+    var hi = []
+    for (var i = 0; i < 13; i++) {
+      hi.push(Color.lch({
+        l: color.lch.l,
+        c: color.lch.c,
+        h: i === 0 ? 0 : (360 / 12 * i)
+      }))
+    }
+    console.log(hi);
+    var h = ['linear-gradient(to right']
+    for (var j = 0; j < hi.length; j++) {
+      h.push(', ', hi[j].hex, ' ', hi[j].lch.h === 0 ? 0 : (hi[j].lch.h/360) * 100, '%')
+    }
+    h.push(')')
+
+    console.log('h', h, h.join(''))
+
+    var gradients = {
+      lch: {
+        l: {
+          'background-image': [
+            'linear-gradient(to right,',
+            l[0].hex, ' 0%, ',
+            l[1].hex, ' ', l[1].lch.l, '%, ',
+            l[2].hex, ' 100%)'
+          ].join('')
+        },
+        c: {
+          'background-image': [
+            'linear-gradient(to right,',
+            c[0].hex, ' 0%, ',
+            c[1].hex, ' ', c[1].lch.l, '%, ',
+            c[2].hex, ' 100%)'
+          ].join('')
+        },
+        h: {
+          'background-image': h.join('')
+        }
+      }
+    }
+    console.log(gradients);
+    return gradients;
+  }
+
   function stylize(color) {
     var pad = 1 + color.lch.c/50
     var margin = ' ' + (pad * -1) + 'em'
@@ -27,11 +103,13 @@ clutch.factory('Anchor', ['Color', function(Color) {
     updateLch: function(newLch) {
       Anchor.color = Color.lch(_.extend(Anchor.color.lch, newLch))
       Anchor.styles = stylize(Anchor.color)
+      Anchor.gradients = createGradients(Anchor.color)
     },
 
     updateRGB: function(newRGB) {
       Anchor.color = Color.rgb(_.extend(Anchor.color.rgb, newRGB))
       Anchor.styles = stylize(Anchor.color)
+      Anchor.gradients = createGradients(Anchor.color)
     },
 
     updateHex: function(newHex) {
