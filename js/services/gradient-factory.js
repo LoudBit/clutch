76c723068121490564f5f0bb1b5ca0cbb9c4dc4d
Darcy Murphy <mrDarcyMurphy@gmail.com>
2016-05-22 12:18:03 -0500

    Add rgb gradients

--- js/services/gradient-factory.js
+++ js/services/gradient-factory.js
@@ -1,7 +1,19 @@
 clutch.factory('Gradients', ['Color', function(Color) {
 
   function createGradients(color) {
+    return {
+      lch: createLChGradients(color),
+      rgb: createRGBGradients(color)
+    }
+  }
 
+  return {
+    create: createGradients
+  }
+
+  // ---
+
+  function createLChGradients(color) {
     function createHue (i) {
       return Color.lch({
         l: color.lch.l,
@@ -13,8 +25,6 @@ clutch.factory('Gradients', ['Color', function(Color) {
     function createHueString(color, i) {
       return [', ', color.hex, ' ', color.lch.h === 0 ? 0 : (color.lch.h/360) * 100, '%'].join('')
     }
-
-    // --
 
     var l = [
       Color.lch({ l: 0, c: color.lch.c, h: color.lch.h }),
@@ -35,32 +45,71 @@ clutch.factory('Gradients', ['Color', function(Color) {
     ]
 
     return {
-      lch: {
-        l: {
-          'background-image': [
-            'linear-gradient(to right,',
-            l[0].hex, ' 0%, ',
-            l[1].hex, ' ', l[1].lch.l, '%, ',
-            l[2].hex, ' 100%)'
-          ].join('')
-        },
-        c: {
-          'background-image': [
-            'linear-gradient(to right,',
-            c[0].hex, ' 0%, ',
-            c[1].hex, ' ', c[1].lch.l, '%, ',
-            c[2].hex, ' 100%)'
-          ].join('')
-        },
-        h: {
-          'background-image': h.join('')
-        }
-      }
+      l: {
+        'background-image': [
+          'linear-gradient(to right,',
+          l[0].hex, ' 0%, ',
+          l[1].hex, ' ', l[1].lch.l, '%, ',
+          l[2].hex, ' 100%)'
+        ].join('')
+      },
+      c: {
+        'background-image': [
+          'linear-gradient(to right,',
+          c[0].hex, ' 0%, ',
+          c[1].hex, ' ', c[1].lch.l, '%, ',
+          c[2].hex, ' 100%)'
+        ].join('')
+      },
+      h: { 'background-image': h.join('') }
     }
   }
 
-  return {
-    create: createGradients
+  function createRGBGradients(color) {
+    var r = [
+      Color.rgb({ r: 0, g: color.rgb.g, b: color.rgb.b }),
+      Color.rgb({ r: 127, g: color.rgb.g, b: color.rgb.b }),
+      Color.rgb({ r: 255, g: color.rgb.g, b: color.rgb.b }),
+    ]
+
+    var g = [
+      Color.rgb({ r: color.rgb.r, g: 0, b: color.rgb.b }),
+      Color.rgb({ r: color.rgb.r, g: 127, b: color.rgb.b }),
+      Color.rgb({ r: color.rgb.r, g: 255, b: color.rgb.b }),
+    ]
+
+    var b = [
+      Color.rgb({ r: color.rgb.r, g: color.rgb.g, b: 0 }),
+      Color.rgb({ r: color.rgb.r, g: color.rgb.g, b: 127 }),
+      Color.rgb({ r: color.rgb.r, g: color.rgb.g, b: 255 }),
+    ]
+
+    return {
+      r: {
+        'background-image': [
+          'linear-gradient(to right,',
+          r[0].hex, ' 0%, ',
+          r[1].hex, ' 50%, ',
+          r[2].hex, ' 100%)'
+        ].join('')
+      },
+      g: {
+        'background-image': [
+          'linear-gradient(to right,',
+          g[0].hex, ' 0%, ',
+          g[1].hex, ' 50%, ',
+          g[2].hex, ' 100%)'
+        ].join('')
+      },
+      b: {
+        'background-image': [
+          'linear-gradient(to right,',
+          b[0].hex, ' 0%, ',
+          b[1].hex, ' 50%, ',
+          b[2].hex, ' 100%)'
+        ].join('')
+      }
+    }
   }
 
 }])
