e05c000a517ce150fb66163d837f33b2e13b2f72
Mr. Darcy Murphy <darcy.murphy@loudbit.com>
2020-01-19 10:35:08 -0600

    rm all

--- js/controllers/color-controller.js
+++
@@ -1,8 +0,0 @@
-/* jshint debug: true */
-clutch.controller('ColorCtrl', ['$scope', 'Color', 'Spectrum', 'Grid', 'Anchor', function($scope, Color, Spectrum, Grid, Anchor) {
-
-  $scope.anchor = Anchor
-
-  return this
-
-}])
--- js/filters/digit-filter.js
+++
@@ -1,7 +0,0 @@
-clutch.filter('toFixed', function() {
-  return function(input, digits) {
-    input = parseFloat(input)
-    digits = parseFloat(digits) || 3
-    return input.toFixed(digits)
-  }
-})
