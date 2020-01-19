039387c9a1b6ea07bb86480b6c0cad5cbcfa2964
Mr. Darcy Murphy <darcy.murphy@loudbit.com>
2020-01-19 12:16:46 -0600

    add and setup inkline

--- pages/index.vue
+++ pages/index.vue
@@ -5,18 +5,7 @@
       <h1 class="title">clutch</h1>
       <h2 class="subtitle">color palette generator</h2>
       <h2 class="subtitle">Background Color: {{ bg }}</h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
-          >Documentation</a
-        >
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-          >GitHub</a
-        >
-        <button @click="randomize" class="button--grey">Randomize</button>
-      </div>
+      <i-button @click="randomize" variant="primary">Randomize</i-button>
     </div>
   </div>
 </template>
@@ -41,36 +30,3 @@ export default {
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
---
+++ plugins/inkline.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import '@inkline/inkline/dist/inkline.css'
+import Inkline from '@inkline/inkline'
+
+Vue.use(Inkline)
