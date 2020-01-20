51a1ead88d3ddae30a53e2bb5cf7c430fdaef1de
Mr. Darcy Murphy <darcy.murphy@loudbit.com>
2020-01-19 18:13:44 -0600

    translate color library from angular to plain js

--- store/palette.js
+++ store/palette.js
@@ -1,25 +1,11 @@
-function Color() {
-  return {
-    name: 'New Color',
-    hex: `#${hex()}${hex()}${hex()}`
-  }
-}
-
-function hex() {
-  let str = Math.floor(Math.random() * Math.floor(256)).toString(16)
-  str = str.length === 1 ? `0${str}` : str
-  return str
-}
+import Color from '@/lib/color/index'
 
 export const state = () => ({
-  background: {
-    name: 'Red',
-    hex: '#ffcccc'
-  }
+  background: Color.random()
 })
 
 export const mutations = {
   randomize(state) {
-    state.background = new Color()
+    state.background = Color.random()
   }
 }
