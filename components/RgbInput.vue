b0688005e88cdbb132f358a7ebdaaf8d03bae872
Mr. Darcy Murphy <darcy.murphy@loudbit.com>
2020-08-30 09:22:07 -0500

    rgb sliders in place

--- components/RgbInput.vue
+++ components/RgbInput.vue
@@ -1,13 +1,31 @@
 <template>
-  <div class="ui row padded">
-    <div class="ui column padded x-33">
-      <input v-model.number="r" size="sm" type="number" min="0" max="255" step="1" />
+  <div class="">
+    <div class="slider-outer-grid">
+      <div class="ui slider slider-inner-grid">
+        <div class="text-center">
+          <label>R</label>
+        </div>
+        <input :value="r" type="range" min="0" max="255" step="1" @input="changeR" />
+      </div>
+      <input :value="r" type="number" min="0" max="255" step="1" @change="changeR" />
     </div>
-    <div class="ui column padded">
-      <input v-model.number="g" size="sm" type="number" min="0" max="255" step="1" />
+    <div class="slider-outer-grid">
+      <div class="ui slider slider-inner-grid">
+        <div class="text-center">
+          <label>G</label>
+        </div>
+        <input :value="g" type="range" min="0" max="255" step="1" @input="changeG" />
+      </div>
+      <input :value="g" type="number" min="0" max="255" step="1" @change="changeG" />
     </div>
-    <div class="ui column padded">
-      <input v-model.number="b" size="sm" type="number" min="0" max="255" step="1" />
+    <div class="slider-outer-grid">
+      <div class="ui slider slider-inner-grid">
+        <div class="text-center">
+          <label>B</label>
+        </div>
+        <input :value="b" type="range" min="0" max="255" step="1" @input="changeB" />
+      </div>
+      <input :value="b" type="number" min="0" max="255" step="1" @change="changeB" />
     </div>
   </div>
 </template>
@@ -28,9 +46,10 @@ export default {
     }
   },
 
-  // TODO: use the same pattern as LchInput -- but which one is correct?
-  watch: {
-    r(r, oldR) {
+  // TODO: use the same pattern as LchInput -- but which one is correct? - LCH!
+  methods: {
+    changeR(event) {
+      const r = parseInt(event.target.value, 10)
       const rgb = this.input.colors[this.colorIndex].rgb
       this.$store.dispatch('palette/updateRgb', {
         colorIndex: this.colorIndex,
@@ -39,22 +58,24 @@ export default {
         rgb: [r, rgb[1], rgb[2]]
       })
     },
-    g(g, oldG) {
-      const rgb = this.input.colors[this.colorIndex].rgb
-      this.$store.dispatch('palette/updateLch', {
-        colorIndex: this.colorIndex,
-        input: this.input,
-        inputIndex: this.inputIndex,
-        lch: [rgb[0], g, rgb[2]]
-      })
-    },
-    b(b, oldB) {
+    changeG(event) {
+      const g = parseInt(event.target.value, 10)
       const rgb = this.input.colors[this.colorIndex].rgb
       this.$store.dispatch('palette/updateRgb', {
         colorIndex: this.colorIndex,
         input: this.input,
         inputIndex: this.inputIndex,
-        lch: [rgb[0], rgb[1], b]
+        rgb: [rgb[0], g, rgb[2]]
+      })
+    },
+    changeB(event) {
+      const b = parseInt(event.target.value, 10)
+      const rgb = this.input.colors[this.colorIndex].rgb
+      this.$store.dispatch('palette/updateRgb', {
+        colorIndex: this.colorIndex,
+        input: this.input,
+        inputIndex: this.inputIndex,
+        rgb: [rgb[0], rgb[1], b]
       })
     }
   }
