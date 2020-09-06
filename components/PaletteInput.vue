6ff56d4694f82eaba2afb46ad057cc089a15c92e
Mr. Darcy Murphy <darcy.murphy@loudbit.com>
2020-09-06 12:55:12 -0500

    a start at the color list

--- components/PaletteInput.vue
+++ components/PaletteInput.vue
@@ -41,19 +41,20 @@
       :input="input"
     ></ColorInput>
     <div class="ui grid grid--add-color">
-      <button class="ui x-100" title="Add Color" @click="addColor()">Add Color</button>
+      <button class="ui x-100" title="Add Color" @click="addColor()">
+        <font-awesome-icon icon="plus" />
+      </button>
     </div>
-    <div class="ui grid grid--palette-swatches">
-      <div v-for="(color, index) in inputColors" :key="index" :style="{ backgroundColor: color.hex() }"></div>
-    </div>
-    <!-- <div class="ui list list--palette-colors">
-      <ul>
-        <li v-for="(color, index) in inputColors" :key="index">
-          <span class="palette-swatch" :style="{ backgroundColor: color.hex() }"></span>
-          {{ color.hex() }}
+    <div>
+      <ul class="ui list list-tight list--palette-colors">
+        <li v-for="(color, index) in inputColors" :key="index" class="ui grid grid--palette-colors">
+          <div class="palette-swatch-cell">
+            <span class="palette-swatch" :style="{ backgroundColor: color.hex() }" :title="color.hex()"></span>
+          </div>
+          <span>{{ color.hex() }}</span>
         </li>
       </ul>
-    </div> -->
+    </div>
   </div>
 </template>
 
@@ -145,7 +146,7 @@ export default {
   grid-template-columns: repeat(8, 1fr);
 }
 .grid--add-color button {
-  grid-column: 1 / 4;
+  grid-column: 1 / 2;
 }
 
 .grid--palette-swatches {
@@ -158,11 +159,40 @@ export default {
   border-radius: 2px;
 }
 
+.palette-swatch-cell {
+  text-align: center;
+}
 .palette-swatch {
   display: inline-block;
   width: 16px;
   height: 16px;
   border: 1px solid rgba(black, 0.1);
   border-radius: 2px;
+  vertical-align: text-bottom;
+}
+
+.ui.list.list--palette-colors {
+  margin: 8px 0 0 0;
+}
+
+.grid--palette-colors {
+  grid-template-columns: repeat(8, 1fr);
+  grid-template-rows: auto;
+  align-content: center;
+}
+.grid--palette-colors > :nth-child(1) {
+  grid-column: 1 / 2;
+}
+.grid--palette-colors > :nth-child(2) {
+  grid-column: 2 / 4;
+}
+.grid--palette-colors > :nth-child(3) {
+  grid-column: 4 / 5;
+}
+.grid--palette-colors > :nth-child(4) {
+  grid-column: 5 / 6;
+}
+.grid--palette-colors > :nth-child(5) {
+  grid-column: 6 / 7;
 }
 </style>
