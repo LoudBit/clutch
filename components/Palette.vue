a471ed317c7f75e1dd87bec3d1740475d7680f1b
Mr. Darcy Murphy <darcy.murphy@loudbit.com>
2020-10-12 09:51:16 -0500

    remove old flexbox wrappers

--- components/Palette.vue
+++ components/Palette.vue
@@ -1,19 +1,13 @@
 <template>
   <div>
     <BG :color="bg"></BG>
-
     <div class="ui grid grid--heading">
       <h1 class="ui h1">Palette</h1>
       <button class="ui small x-100" title="Add Input" @click="addInput">Add Palette</button>
     </div>
-
-    <div class="ui row">
-      <div class="ui column">
-        <template v-for="(input, index) in rawInputs">
-          <PaletteInput :key="`input-${input.id}`" :input="input" :index="index"></PaletteInput>
-        </template>
-      </div>
-    </div>
+    <template v-for="(input, index) in rawInputs">
+      <PaletteInput :key="`input-${input.id}`" :input="input" :index="index"></PaletteInput>
+    </template>
   </div>
 </template>
 
